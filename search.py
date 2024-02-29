bebf8e531465400503b302c40260621ad06dc288
Jason <ffuup35452@outlook.com>
2024-02-28 23:33:01 -0500

    greedy_bfs

--- search.py
+++ search.py
@@ -50,6 +50,16 @@ def is_enclosed(point, polygon_list):
             return True
     return False
 
+def is_turf(point, polygon_list):
+    #print(f"({point.x}, {point.y})")
+    point = geometry.Point(point.x, point.y)
+    for polygon in polygon_list:
+        enclosure = geometry.Polygon(polygon)
+        buffered_enclosure = enclosure.buffer(0.2)
+        if buffered_enclosure.contains(point) or point.x < 0 or point.x >= 50 or point.y < 0 or point.y >= 50: # the point can also be outside the canvas
+            return True
+    return False
+
 def actions_bfs_dfs(point, dest, enc_vertices, explored):
     children = []
     
@@ -148,6 +158,46 @@ def depth_first_search(source, dest, enc_vertices):
                 frontier.push(child)
             explored.append(child)
 
+# Greedy Best-First Search implementation taking a source point, destination point, and the enclosed polygons
+def greedy_bfs(source, dest, enc_vertices):
+    name = "Breadth First Search"
+    nodes_expanded = 0
+    path_cost = 0
+    node = source
+    node.heuristic = distance(source, dest)
+
+    # return here
+    if source.__eq__(dest):
+        print_to_summary(name, path_cost, nodes_expanded)
+        return reconstruct_solution_path(child)
+
+    frontier = PriorityQueue()
+    explored = [] # i couldn't use a set bc Point isn't iterable
+    frontier.push(node, node.heuristic)
+    explored.append(node)
+
+    while True:
+        if frontier.isEmpty():
+            #failure
+            return False
+        
+        node = frontier.pop()
+        #print(f"{nodes_expanded}")
+        nodes_expanded += 1
+
+        actions = actions_bfs_dfs(node, dest, enc_vertices, explored)
+        node.set_children(actions)
+        explored.append(node)
+
+        for child in node.children:
+            if child.__eq__(dest):
+                SOLUTION = reconstruct_solution_path(child)
+                print_to_summary(name, len(SOLUTION)-1, nodes_expanded)
+                return SOLUTION
+            elif child not in explored:
+                frontier.push(child, child.heuristic)
+            explored.append(child)
+
 if __name__ == "__main__":
     epolygons = gen_polygons('TestingGrid/world1_enclosures.txt')
     tpolygons = gen_polygons('TestingGrid/world1_turfs.txt')
@@ -157,7 +207,7 @@ if __name__ == "__main__":
     source = Point(8,10)
     dest = Point(43,45)
 
-    enc_polgons = []
+    enc_polygons = []
     tur_polygons = []
 
     fig, ax = draw_board()
@@ -172,7 +222,7 @@ if __name__ == "__main__":
         for p in polygon:
             draw_point(ax, p.x, p.y)
             shape.append((p.x, p.y))
-        enc_polgons.append(shape)
+        enc_polygons.append(shape)
 
     for polygon in epolygons:
         for i in range(0, len(polygon)):
@@ -187,8 +237,9 @@ if __name__ == "__main__":
             draw_green_line(ax, [polygon[i].x, polygon[(i+1)%len(polygon)].x], [polygon[i].y, polygon[(i+1)%len(polygon)].y])
 
     #### Here call your search to compute and collect res_path
-    res_path = breadth_first_search(source, dest, enc_polgons)
-    #res_path = depth_first_search(source, dest, enc_polgons)
+    #res_path = breadth_first_search(source, dest, enc_polygons)
+    #res_path = depth_first_search(source, dest, enc_polygons)
+    res_path = greedy_bfs(source, dest, enc_polygons)
 
     # res_path = [Point(24,17), Point(25,17), Point(26,17), Point(27,17),  
     #             Point(28,17), Point(28,18), Point(28,19), Point(28,20)]
